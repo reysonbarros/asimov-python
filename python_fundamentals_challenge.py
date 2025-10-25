af5fd6ebe18d49e08c68311d62554ea4bc1af12d
Reyson Barros <reyson_barros@hotmail.com>
2025-10-24 22:50:49 -0300

    Using f-strings

--- python_fundamentals_challenge.py
+++ python_fundamentals_challenge.py
@@ -4,9 +4,9 @@ Output: Greeting message, name's length and age(5 years later)
 '''
 name = str(input("Type your name:"))
 age = int(input("Type your age:"))
-print("Hello "+name+" !!!")
-print("Your name has "+str(len(name))+" characters.")
-print("After 5 years you will be "+str(age+5)+" years old.")
+print(f"Hello {name} !!!")
+print(f"Your name has {str(len(name))} characters.")
+print(f"After 5 years you will be {str(age+5)} years old.")
 
 
 
