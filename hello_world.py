28f6092a51aebaa53324c3d36cdaed5e426cabe9
Reyson Barros <reyson_barros@hotmail.com>
2025-10-24 22:42:55 -0300

    First steps using Python

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+print("Hello World!!!")
+# It's a comment single line. This line will be ignored by python compiler
+'''
+It's
+a
+comment
+multiple
+lines
+'''
+x = 1
+y = 7
+print('x='+str(x))
+print('y='+str(y))
+print('The sum x + y is equal to '+str(int(x+y)))
+
---
+++ python_fundamentals_challenge.py
@@ -0,0 +1,12 @@
+'''
+Input: name and age
+Output: Greeting message, name's length and age(5 years later)
+'''
+name = str(input("Type your name:"))
+age = int(input("Type your age:"))
+print("Hello "+name+" !!!")
+print("Your name has "+str(len(name))+" characters.")
+print("After 5 years you will be "+str(age+5)+" years old.")
+
+
+
