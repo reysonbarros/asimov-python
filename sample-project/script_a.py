1a6a3c9b4e00109174c49b62de5a8507ed69c80f
Reyson Barros <reyson_barros@hotmail.com>
2025-11-24 21:24:14 -0300

    Samples using __name__ variable

---
+++ sample-project/script_a.py
@@ -0,0 +1,3 @@
+from script_b import x
+
+print(f"X={x}")
---
+++ sample-project/script_b.py
@@ -0,0 +1,7 @@
+x = 1
+
+if __name__ == '__main__':
+    print(__name__)
+
+# __name__ equals "__main__" when the script perform alone
+# __name__ equals "nome do script" when the script is imported
